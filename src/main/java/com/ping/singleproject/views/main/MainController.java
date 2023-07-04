f2de941a9b8aa600b33c179a75d6fc2a34352ea0
Pinguri <teasd777@naver.com>
2023-07-04 16:00:51 +0900

    중간 커밋

--- src/main/java/com/ping/singleproject/views/main/MainController.java
+++ src/main/java/com/ping/singleproject/views/main/MainController.java
@@ -1,4 +1,22 @@
 package com.ping.singleproject.views.main;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value = "/main")
+@Slf4j
+@RequiredArgsConstructor
 public class MainController {
+
+    @GetMapping(value = "")
+    public ModelAndView MainPage(){
+        return new ModelAndView("/main/main");
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/main/main.jsp
@@ -0,0 +1,4 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div>
+메인페이지
+</div>
