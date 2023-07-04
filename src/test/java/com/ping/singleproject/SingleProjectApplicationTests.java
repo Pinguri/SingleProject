6151bdd82c59a16bf0ef4a0fdfb76d5b5d331281
Pinguri <teasd777@naver.com>
2023-07-04 14:38:40 +0900

    first commit

---
+++ src/test/java/com/ping/singleproject/SingleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ping.singleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
